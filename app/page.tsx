dae24377b18a3c20c1dd2dd3d9a71929e6410015
Aayush Badoni <a.badoni@Aayushs-MacBook-Pro-2.local>
2024-02-28 23:32:27 +0530

    fixed build issues related to client side rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,25 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import {
-  PricesDisplay,
-  TrendingCoins,
-  NavBar,
-  Carousel,
-  GetStarted,
-  CarouselSec,
-} from "./components";
+import dynamic from "next/dynamic";
+
+// Import components dynamically
+const PricesDisplay = dynamic(() => import("./components/PricesDisplay"));
+const TrendingCoins = dynamic(() => import("./components/TrendingCoins"));
+const NavBar = dynamic(() => import("./components/NavBar"));
+const GetStarted = dynamic(() => import("./components/GetStarted"));
+const CarouselSec = dynamic(() => import("./components/CarouselSec"));
 
 export default function Home() {
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [windowWidth, setWindowWidth] = useState(0);
+  const [isClient, setIsClient] = useState(false);
 
   useEffect(() => {
+    setIsClient(true);
     const handleResize = () => {
       setWindowWidth(window.innerWidth);
     };
 
+    handleResize(); // Initial window width
     window.addEventListener("resize", handleResize);
 
     return () => {
@@ -26,36 +29,26 @@ export default function Home() {
 
   return (
     <>
-      <NavBar />
+      {isClient && <NavBar />}
       {windowWidth >= 550 ? (
         <div className="flex flex-col">
           <div className="flex flex-row w-full pl-8 pr-8">
-            <div className="flex-grow">
-              <PricesDisplay />
-            </div>
+            <div className="flex-grow">{isClient && <PricesDisplay />}</div>
             <div className="flex flex-col w-[25%] hidden lg:flex">
-              <GetStarted />
-              <TrendingCoins />
+              {isClient && <GetStarted />}
+              {isClient && <TrendingCoins />}
             </div>
           </div>
 
-          <div>
-            <CarouselSec />
-          </div>
+          <div>{isClient && <CarouselSec />}</div>
         </div>
       ) : (
         <div className="flex flex-col">
-          <div className="ml-4">
-            <PricesDisplay />
-          </div>
-          <div className="mb-20">
-            <CarouselSec />
-          </div>
+          <div className="ml-4">{isClient && <PricesDisplay />}</div>
+          <div className="mb-20">{isClient && <CarouselSec />}</div>
           <div className="flex flex-col w-full">
-            <div className="mx-4 mb-4">
-              <GetStarted />
-            </div>
-            <TrendingCoins />
+            <div className="mx-4 mb-4">{isClient && <GetStarted />}</div>
+            {isClient && <TrendingCoins />}
           </div>
         </div>
       )}
