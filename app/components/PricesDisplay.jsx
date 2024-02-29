593e1cbf8f57106e39acaa7e7ab4408c722c10ce
Aayush Badoni <a.badoni@Aayushs-MacBook-Pro-2.local>
2024-02-29 13:48:13 +0530

    added invalid url error handling

--- app/components/PricesDisplay.jsx
+++ app/components/PricesDisplay.jsx
@@ -1,17 +1,18 @@
-"use client"
 import React, { useEffect, useState } from 'react';
 import { fetchCoinPrices } from '../utils/fetchCoinPrices';
 import { coinData } from '../utils/coinData';
 import { numberWithCommas } from '../utils/numberWithCommas';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCaretUp } from '@fortawesome/free-solid-svg-icons';
+import { faCaretUp, faAnglesRight, faHourglass1 } from '@fortawesome/free-solid-svg-icons';
 import TradingViewWidget from './TradingViewWidget';
 import styles from '../styles/PricesDisplayStyles.module.css';
-
+import { useRouter } from "next/navigation";
 
 const PricesDisplay = ({ token }) => {
   const [coin, setCoin] = useState(null);
   const [prices, setPrices] = useState(null);
+  const [error, setError] = useState(false);
+  const router = useRouter();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -20,6 +21,7 @@ const PricesDisplay = ({ token }) => {
         setCoin(info);
       } catch (error) {
         console.error('Error fetching coin data:', error);
+        setError(true); // Set error state to true if an error occurs
       }
     };
     fetchData();
@@ -32,38 +34,61 @@ const PricesDisplay = ({ token }) => {
         setPrices(data);
       } catch (error) {
         console.error('Error fetching data:', error);
+        setError(true); // Set error state to true if an error occurs
       }
     };
 
     fetchData();
   }, [token]);
 
+  // If error state is true, send to 404 page
+  if (error) {
+    router.push("/404");
+  }
+
+  // If coin object is still null, return null (waiting for data to load)
+  if (!coin) {
+    return null;
+  }
 
   return (
     <>
-      {/* For mobile */}
-      <div className={`${styles.mobile_show} flex flex-row items-center mb-4`}>
+      <div className='flex row mb-4 items-center ml-1 gap-2'>
+        <div className='font-light text-gray-700'>
+          Cyrptocurrencies
+        </div>
         <div>
-          <img className="h-10" src={coin && coin.image.thumb} alt={`${token} Logo`} />
+          <img src="angle.svg" alt="Right Arrow" />
+        </div>
+        <div className='text-black'>
+          <a href={`/${token}`}>
+            {token.charAt(0).toUpperCase() + token.slice(1)}
+          </a>
+        </div>
+      </div>
+      {/* For mobile */}
+      <div className={`${styles.mobile_show} ml-1 flex flex-row items-center mb-4`}>
+        <div>
+          <img className="h-10" src={coin.image.thumb} alt={`${token} Logo`} />
         </div>
 
         <div className="text-2xl font-medium ml-3">{token.charAt(0).toUpperCase() + token.slice(1)}</div>
-        <div className="ml-3 text-gray-500 font-medium"> {coin && coin.symbol && coin.symbol.toUpperCase()} </div>
+        <div className="ml-3 text-gray-500 font-medium"> {coin.symbol.toUpperCase()} </div>
         <div className="py-4 py-[6px] px-2 ml-8 mr-2 bg-gray-500 font-light text-white rounded-lg">
-          {coin && `Rank #${coin.market_cap_rank}`}
+          {`Rank #${coin.market_cap_rank}`}
         </div>
       </div>
 
       <div className="bg-white mr-4 flex flex-col justify-center rounded-lg shadow-md p-6 border-2">
         <div className={`${styles.mobile} flex flex-row items-center mb-10`}>
           <div>
-            <img className="h-10" src={coin && coin.image.thumb} alt={`${token} Logo`} />
+            <img className="h-10" src={coin.image.thumb} alt={`${token} Logo`} />
           </div>
 
           <div className="text-2xl font-medium ml-3">{token.charAt(0).toUpperCase() + token.slice(1)}</div>
-          <div className="ml-3 text-gray-500 font-medium"> {coin && coin.symbol && coin.symbol.toUpperCase()} </div>
+          <div className="ml-3 text-gray-500 font-medium"> {coin.symbol.toUpperCase()} </div>
           <div className="py-4 py-[6px] px-2 ml-10 bg-gray-500 font-light text-white rounded-lg">
-            {coin && `Rank #${coin.market_cap_rank}`}
+            {`Rank #${coin.market_cap_rank}`}
           </div>
         </div>
 
@@ -96,12 +121,11 @@ const PricesDisplay = ({ token }) => {
           <div className='border-[1px] rounded-lg font-extralight mt-6 mb-6'></div>
           <p className='text-black font-semibold mb-6'>{token.charAt(0).toUpperCase() + token.slice(1)} Price Chart (USD)</p>
           <div className='h-[47vh]'>
-            {coin?.symbol && <TradingViewWidget symbol={coin.symbol} />}
+            {coin.symbol && <TradingViewWidget symbol={coin.symbol} />}
           </div>
         </div>
       </div>
     </>
-
   );
 };
 
